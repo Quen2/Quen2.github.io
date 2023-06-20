fa72c9576a5fae1f05ff46ef7958858e683100e5
Quen2 <ZexFrost1@gmail.com>
2023-06-20 22:16:05 +0200

    [~] First Project Structure, with tailwind activated

---
+++ my_portfolio/src/Views/Index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Index()
+{
+    return (
+        <div>
+            <h1 className="text-red-500">
+                Vous avez les cramptés ?
+            </h1>
+        </div>
+    )
+}
