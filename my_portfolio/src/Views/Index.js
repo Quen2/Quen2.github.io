0e07c65716e253b9e06a928f8a8a54427d92abd3
Quen2 <ZexFrost1@gmail.com>
2023-06-20 23:00:35 +0200

    [~] Add babel private property in package.json

--- my_portfolio/src/Views/Index.js
+++ my_portfolio/src/Views/Index.js
@@ -20,7 +20,7 @@ export default function Index()
         <div className="w-screen h-screen flex bg-stone-800">
             <div className=" m-auto self-center w-1/2 h-1/2 bg-stone-600 rounded-3xl border border-stone-900">
                 <h1 className="text-center mt-5 text-white title">Bonjour</h1>
-                <img src={pp} className="w-1/4 h-1/2 rounded-full mx-auto my-5 object-cover anim imgMaker"></img>
+                <img src={pp} className="w-1/4 h-1/2 rounded-full mx-auto my-5 object-cover anim imgMaker" alt="profilePicture"></img>
                 <p className="text-3xl text-white text-center">Ouais, c'est <span className="title">G</span>reg</p>
                 <button className="w-fit h-auto  bg-stone-400 text-white" onClick={handleClick}>Me découvrir</button>
             </div>
