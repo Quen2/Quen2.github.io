53918d3f1a6b2a825d4e4a5e21f2131180a1d8a5
Quen2 <ZexFrost1@gmail.com>
2023-06-20 22:48:19 +0200

    [~] First View is done, short sentence to describe myself, working on the transition between this view and my table of content

--- my_portfolio/src/Views/Index.js
+++ my_portfolio/src/Views/Index.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import pp from "../Assets/pp.jpg"
+import '../Scripts/Img.script'
 
-export default function Index()
+export default function Index() 
 {
+    const [state, setState] = useState(null)
+
+    function handleClick(e) 
+    {
+        e.target.parentNode.classList.add('zoom');
+        setTimeout(() => {
+            setState('home')
+        }, 2000);
+    }
+
     return (
-        <div>
-            <h1 className="text-red-500">
-                Vous avez les cramptés ?
-            </h1>
+        state === null
+        ?
+        <div className="w-screen h-screen flex bg-stone-800">
+            <div className=" m-auto self-center w-1/2 h-1/2 bg-stone-600 rounded-3xl border border-stone-900">
+                <h1 className="text-center mt-5 text-white title">Bonjour</h1>
+                <img src={pp} className="w-1/4 h-1/2 rounded-full mx-auto my-5 object-cover anim imgMaker"></img>
+                <p className="text-3xl text-white text-center">Ouais, c'est <span className="title">G</span>reg</p>
+                <button className="w-fit h-auto  bg-stone-400 text-white" onClick={handleClick}>Me découvrir</button>
+            </div>
         </div>
-    )
+        :
+        <div className="flex flex-col background animate-fade w-screen h-screen">
+            <h1>Oui</h1>
+        </div>
+    )    
 }
+
