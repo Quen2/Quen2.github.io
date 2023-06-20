4249ef09e19c8557be05ebff32c934068973d9e2
Quen2 <ZexFrost1@gmail.com>
2023-06-20 17:22:20 +0200

    [~] Fix babel dependencies

--- my_portfolio/src/Views/Index.js
+++ my_portfolio/src/Views/Index.js
@@ -1,14 +1,11 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import TableOfContent from "./TableOfContent";
 import pp from "../Assets/pp.jpg"
 import '../Scripts/imgManager.script'
-// import '../Scripts/slide.script'
-// import '../Scripts/index.script'
 
 export default function Index() 
 {
     const [state, setState] = useState(null)
-    const [scroll, setScroll] = useState(null);
 
     function handleClick(e) 
     {
@@ -24,13 +21,13 @@ export default function Index()
         <div className="w-screen h-screen flex bg-stone-800">
             <div className=" m-auto self-center w-1/2 h-1/2 bg-stone-600 rounded-3xl border border-stone-900">
                 <h1 className="text-center mt-5 text-white title">Bonjour</h1>
-                <img src={pp} className="w-1/4 h-1/2 rounded-full mx-auto my-5 object-cover anim imgMaker"></img>
+                <img src={pp} className="w-1/4 h-1/2 rounded-full mx-auto my-5 object-cover anim imgMaker" alt="profilePicture"></img>
                 <p className="text-3xl text-white text-center">Ouais, c'est <span className="title">G</span>reg</p>
                 <button className="w-fit h-auto  bg-stone-400 text-white" onClick={handleClick}>Me découvrir</button>
             </div>
         </div>
         :
-        <div className="flex flex-col background animate-fade w-screen h-screen">
+        <div className="flex flex-col background animate-fade w-screen h-screen bg-stone-800">
             <TableOfContent />
         </div>
     )    
