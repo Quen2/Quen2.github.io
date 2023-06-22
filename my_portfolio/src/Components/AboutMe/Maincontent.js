bc530a99c01e72194bd21f131fde62f226a7352a
Quen2 <ZexFrost1@gmail.com>
2023-06-23 01:17:55 +0200

    [~] Structure for the about me is nearly done

--- my_portfolio/src/Components/AboutMe/Maincontent.js
+++ my_portfolio/src/Components/AboutMe/Maincontent.js
@@ -6,7 +6,17 @@ export default function MainContent ()
         <div className="pt-10 bg-stone-600">
             <div>
                 <h1 className="text-white text-center text-3xl font-PlayFair">A propos de moi</h1>
-
+                <div className="flex flex-col">
+                    <div>
+                        <h1>Info 1</h1>
+                    </div>
+                    <div>
+                        <h1>Info 2</h1>
+                    </div>
+                    <div>
+                        <h1>Info 3</h1>
+                    </div>
+                </div>
             </div>
         </div>
     )
