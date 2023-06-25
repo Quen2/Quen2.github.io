383d79fb6ba66f433b9eb5710965ea154c1fe30e
Quen2 <ZexFrost1@gmail.com>
2023-06-25 16:15:43 +0200

    [~] Versatile skills are done, working on project section

--- my_portfolio/src/Components/AboutMe/Maincontent.js
+++ my_portfolio/src/Components/AboutMe/Maincontent.js
@@ -3,7 +3,6 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../../Scripts/Scroll.script"
 import webAc from "../../Assets/webAca.png"
-import e2c from "../../Assets/e2c.png"
 import nurserie from "../../Assets/nurserie.png"
 import design from "../../Assets/logo_lignes.svg"
 import html from "../../Assets/html5.svg"
@@ -35,18 +34,21 @@ export default function MainContent ()
 
                 <div className="flex flex-col elements p-10">
                     
-                <Carousel showThumbs={false}>
+                <Carousel showThumbs={false} className="">
+
 
                     <div className="flex flex-col lg:flex-row m-5 gap-4 ">
-                        
+                    
                         <div className="border flex flex-col lg:w-3/4">
+                    
+                        <h3 className="text-3xl font-PlayFair my-4 text-stone-700">Mon parcours</h3>
                             
                             <div className="border md:border-none">
                                 <div className="flex gap-8 p-2 text-base lg:text-2xl text-stone-700 font-Indie">
                                     <p className="self-center">Novembre 2022 - Novembre 2024</p>
                                     <h3>Web@cadémie par Epitech</h3>
                                 </div>
-                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                <div className="p-2 text-xs lg:text-lg mb-2 font-PlayFair">
                                     <p>-Formation concentrée sur le développement Web. </p>
                                     <p>-Pédagogie par projet : résolution de problèmes, de cahier des charges, afin de produire un projet concret </p>
                                     <p>Exemples de projet : My_twitter : reproduire les fonctionnalités de base de Twitter (tweet, retweet, follow...) par groupe de 4 en php + utilisation de Tailwind</p>
@@ -58,7 +60,7 @@ export default function MainContent ()
                                     <p className="self-center">Mai 2022 - Septembre 2022</p>
                                     <h3>Ecole de la 2ème chance à Roubaix</h3>
                                 </div>
-                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                <div className="p-2 text-xs lg:text-lg mb-2 font-PlayFair">
                                     <p>-Initiation aux métiers du numériques (développeur web, mais aussi monteur vidéo, développeur de jeux vidéos, ingénieur électronique ) </p>
                                 </div>
                             </div>
@@ -68,7 +70,7 @@ export default function MainContent ()
                                     <p className="self-center">Avril 2022 - Mai 2022 </p>
                                     <h3>Nurserie Numérique à Lomme</h3>
                                 </div>
-                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                <div className="p-2 text-xs lg:text-lg mb-2 font-PlayFair">
                                     <p>-Initiation au métier de développeur web : Découverte de méthode agile (la méthode Scrum), utilisation de Figma et Miro pour l'UX,ainsi que Trello afin de s'organiser</p>
                                     <p>-Initiation aux bases du développement : HTML, CSS, JS, SQL,  début d'un workflow Git avec 10 participants (afin de gérer les conflits)</p>
                                 </div>
@@ -79,7 +81,7 @@ export default function MainContent ()
                                     <p className="self-center">Octobre 2020 - Octobre 2021</p>
                                     <h3>Formation de Design en distanciel </h3>
                                 </div>
-                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                <div className="p-2 text-xs lg:text-lg mb-2 font-PlayFair">
                                     <p>-Découverte de la perspective avec projet (comme par exemple devoir dessiner l'intérieur d'une chambre)</p>
                                     <p>-Découverte de l'Histoire de l'Art, basé sur le système de MOOC (Massive open online course)</p>
                                 </div>
@@ -94,7 +96,6 @@ export default function MainContent ()
                         
                         <div className="border lg:w-1/4 text-center flex flex-col justify-evenly">
                             <img className="aboutImg mb-2 p-2" src={webAc} alt="logo-webacademie"></img>
-                            <img className="aboutImg mx-auto p-2" src={e2c} alt="logo-e2c"></img>
                             <img className="aboutImg p-2 h-28 mx-auto" src={nurserie} alt="logo-nurserie"></img>
                             <img className="aboutImg p-2 h-28 mx-auto" src={design} alt="logo-formation"></img>
                         </div>
@@ -105,7 +106,10 @@ export default function MainContent ()
 
                         <div className="border flex flex-col lg:w-1/4">
                             
-                        <div className="border md:border-none">
+                        <h3 className="text-3xl font-PlayFair my-4 text-stone-700">Mes compétences</h3>
+
+
+                            <div className="border md:border-none">
                                 <div className="flex gap-8 p-2 text-base lg:text-2xl text-stone-700 font-Indie text-center">
                                     <p>Languages utilisés</p>
                                 </div>
@@ -116,12 +120,18 @@ export default function MainContent ()
 
                             <div className="border md:border-none">
                                 <div className="flex gap-8 p-2 text-lg lg:text-2xl text-stone-700 font-Indie">
-                                    <p className="self-center">Frameworks utilisés</p>
+                                    <p className="self-center">Frameworks utilisés : </p>
+                                </div>
+                                <div className="flex gap-8 p-2 text-lg lg:text-2xl text-stone-700 font-Indie">
+                                    <p className="self-center">-Frameworks orientés Front-End : </p>
                                 </div>
                                 <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
-                                    <p>-Frameworks orientés Front-End :</p>
                                     <p>Bootstrap | Tailwind | React | React Native | Angular</p>
-                                    <p>-Frameworks orientés Back-End :</p>
+                                </div>
+                                <div className="flex gap-8 p-2 text-lg lg:text-2xl text-stone-700 font-Indie">
+                                    <p className="self-center">-Frameworks orientés Back-End : </p>
+                                </div>
+                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
                                     <p>Node.js | Laravel | Symfony</p>
                                 </div>
                             </div>
@@ -145,37 +155,37 @@ export default function MainContent ()
                             </div>
 
                         </div>
-                        <div className="border lg:w-3/4 flex flex-col justify-around gap-4">
+                        <div className="border lg:w-3/4 flex flex-col justify-around gap-4 lg:gap-8">
                             
-                            <div className="flex justify-around self-center lg:gap-20 p-2">
-                                <img src={html} className="w-12 h-12" alt="icon-HTML"></img>
-                                <img src={css} className="w-12 h-12" alt="icon-CSS"></img>
-                                <img src={js} className="w-12 h-12" alt="icon-JS"></img>
-                                <img src={php} className="w-12 h-12" alt="icon-PHP"></img>
+                            <div className="flex justify-around gap-3 self-center lg:gap-20">
+                                <img src={html} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-HTML"></img>
+                                <img src={css} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-CSS"></img>
+                                <img src={js} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-JS"></img>
+                                <img src={php} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-PHP"></img>
                             </div>
 
-                            <div className="flex justify-around self-center lg:gap-20 p-2">
-                                <img src={bootstrap} className="w-12 h-12" alt="icon-bootstrap"></img>
-                                <img src={tailwind} className="w-12 h-12" alt="icon-tailwind"></img>
-                                <img src={react} className="w-12 h-12" alt="icon-react"></img>
-                                <img src={reactNative} className="w-12 h-12" alt="icon-react-native"></img>
-                                <img src={angular} className="w-12 h-12" alt="icon-angular"></img>
+                            <div className="flex justify-around self-center lg:gap-20">
+                                <img src={bootstrap} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-bootstrap"></img>
+                                <img src={tailwind} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-tailwind"></img>
+                                <img src={react} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-react"></img>
+                                <img src={reactNative} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-react-native"></img>
+                                <img src={angular} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-angular"></img>
                             </div>
 
-                            <div className="flex justify-around self-center lg:gap-20 p-2">
-                                <img src={nodeJs} className="w-12 h-12" alt="icon-NodeJs"></img>
-                                <img src={laravel} className="w-12 h-12" alt="icon-Laravel"></img>
-                                <img src={symfony} className="w-12 h-12" alt="icon-Symfony"></img>
+                            <div className="flex justify-around self-center gap-3 lg:gap-20">
+                                <img src={nodeJs} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-NodeJs"></img>
+                                <img src={laravel} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-Laravel"></img>
+                                <img src={symfony} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-Symfony"></img>
                             </div>
 
-                            <div className="flex justify-around self-center lg:gap-20 p-2">
-                                <img src={sql} className="w-12 h-12" alt="icon-SQL"></img>
-                                <img src={mongoDb} className="w-12 h-12" alt="icon-mongoDb"></img>
+                            <div className="flex justify-around self-center lg:gap-20">
+                                <img src={sql} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-SQL"></img>
+                                <img src={mongoDb} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-mongoDb"></img>
                             </div>
 
-                            <div className="flex justify-around self-center lg:gap-20 p-2">
-                                <img src={gimp} className="w-12 h-12" alt="icon-Gimp"></img>
-                                <img src={postman} className="w-12 h-12" alt="icon-Postman"></img>
+                            <div className="flex justify-around self-center lg:gap-20">
+                                <img src={gimp} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-Gimp"></img>
+                                <img src={postman} className="w-10 h-10 lg:w-20 lg:h-20" alt="icon-Postman"></img>
                             </div>
 
                         </div>
@@ -184,11 +194,55 @@ export default function MainContent ()
 
                     <div className="flex flex-col lg:flex-row m-5 gap-4">
 
-                        <div className="border flex flex-col lg:w-3/4">
-                            <h1>Info 1</h1>
-                        </div>
-                        <div className="border lg:w-1/4">
-                            <h2>Content 1</h2>
+                        <div className="border flex flex-col lg:w-full">
+                            
+                            <h3 className="text-3xl font-PlayFair my-4 text-stone-700">Mes compétences Polyvalentes</h3>
+
+                            <div className="border md:border-none">
+                                <div className="flex gap-8 p-2 text-base lg:text-2xl text-stone-700 font-Indie text-center">
+                                    <p>Gestion de projet</p>
+                                </div>
+                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                    <p>Pédagogie par Projets : Cahier des charges à remplir avec une deadline, points réguliers avec l'avancement du projet. Utilisation de la méthode agile Scrum</p>
+                                </div>
+                            </div>
+
+                            <div className="border md:border-none">
+                                <div className="flex gap-8 p-2 text-base lg:text-2xl text-stone-700 font-Indie text-center">
+                                    <p>Résolution de problèmes</p>
+                                </div>
+                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                    <p>Résolution des charges demandées par le client, avec parfois des restrictions de techno, de fonctions à utiliser.</p>
+                                </div>
+                            </div>
+
+                            <div className="border md:border-none">
+                                <div className="flex gap-8 p-2 text-base lg:text-2xl text-stone-700 font-Indie text-center">
+                                    <p>Collaboration | travail d'équipe & communication</p>
+                                </div>
+                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                    <p>Projets collaboratifs : Organisation par Trello, utilisation de Git. Discussions sur d'éventuels problèmes afin de les résoudre ensemble</p>
+                                </div>
+                            </div>
+
+                            <div className="border md:border-none">
+                                <div className="flex gap-8 p-2 text-base lg:text-2xl text-stone-700 font-Indie text-center">
+                                    <p>Rigueur & attention aux détails</p>
+                                </div>
+                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                    <p>Respect des normes demandées : JsLint, W3C. Tests afin de garantir la fiabilité du projet </p>
+                                </div>
+                            </div>
+
+                            <div className="border md:border-none">
+                                <div className="flex gap-8 p-2 text-base lg:text-2xl text-stone-700 font-Indie text-center">
+                                    <p>Création d'une documentation</p>
+                                </div>
+                                <div className="p-2 text-sm lg:text-lg mb-2 font-PlayFair">
+                                    <p>Création de Readme afin de documenter le projet au vu d'un potentiel nouveau développeur</p>
+                                </div>
+                            </div>
+
                         </div>
                     
                     </div>
